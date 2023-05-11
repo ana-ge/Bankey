d39a0a4fcf1393eb1465e0c977dbcd4ccea59cf4
ana-ge <ge.anatoliy@gmail.com>
2023-05-11 14:18:47 +0500

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Анатолий Ге on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
