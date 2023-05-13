181b6c7aea0e8d302003c5f79bf5cc8fe163f3df
ana-ge <ge.anatoliy@gmail.com>
2023-05-13 14:13:40 +0500

    Added delegate methods

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -5,17 +5,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
+    let loginViewController = LoginViewController()
+    let onboardingContainerViewController = OnboardingContainerViewController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        //window?.rootViewController = LoginViewController()
-        window?.rootViewController = OnboardingContainerViewController()
-        //window?.rootViewController = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in the 80s.")
+        
+        loginViewController.delegate = self
+        onboardingContainerViewController.delegate = self
+        
+        window?.rootViewController = loginViewController
 
         return true
     }
-
-
 }
 
+extension AppDelegate: LoginViewControllerDelegate {
+    func didLogin() {
+        window?.rootViewController = onboardingContainerViewController
+    }
+}
+
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+    func didFinishOnboarding() {
+        print("kkk")
+    }
+}
