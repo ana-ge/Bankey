b63a9f855f3bde1ba39a17d85cd64f77e9c71a56
ana-ge <ge.anatoliy@gmail.com>
2023-05-13 16:09:19 +0500

    Added delegate for LogOut and fixed bug with activity indicator

--- Bankey/Login/LoginViewController.swift
+++ Bankey/Login/LoginViewController.swift
@@ -1,5 +1,8 @@
 import UIKit
 
+protocol LogoutDelegate: AnyObject {
+    func didLogout()
+}
 
 protocol LoginViewControllerDelegate: AnyObject {
     func didLogin()
@@ -26,6 +29,11 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         style()
         layout()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        signInButton.configuration?.showsActivityIndicator = false
     }
 }
 
